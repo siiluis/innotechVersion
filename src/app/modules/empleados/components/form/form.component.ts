4fbe9cfd42df8f3180cdf9d47757aa8097beca14
Sara Mejia Moreno <sara.mejiam@perficient.com>
2021-09-23 12:24:58 -0500

    CAmbios a modulo empleados de lógica

--- src/app/modules/empleados/components/form/form.component.ts
+++ src/app/modules/empleados/components/form/form.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { EmpleadosService } from '../../empleados.service';
+import { createForm, Empleados } from '../../models/empleados.model';
 
 @Component({
-  selector: 'app-form',
+  selector: 'add-empleados',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.css']
+  styleUrls: ['./form.component.css'],
 })
-export class FormComponent implements OnInit {
+export class FormAddComponent implements OnInit {
+  idEmpleados: string | undefined = undefined;
+  empleadosForm: FormGroup;
+  btnText: string = 'Guardar';
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private empleadosService: EmpleadosService,
+    private route: ActivatedRoute
+  ) {
+    this.route.params.subscribe((params) => {
+      if (params.id) {
+        this.idEmpleados = params.id;
+        this.btnText = 'Actualizar';
+      }
+    });
+    this.empleadosForm = createForm(new Empleados());
   }
 
+  ngOnInit(): void {
+    if (this.idEmpleados) {
+      this.empleadosService
+        .getEmpleados()
+        .subscribe((response: any) => {
+          this.empleadosForm = createForm(response.data);
+        });
+    }
+  }
+
+  add() {
+    if (this.idEmpleados) {
+      this.empleadosService.updateEmpleados(this.empleadosForm.value);
+    } else {
+      this.empleadosService.addEmpleados(this.empleadosForm.value);
+    }
+  }
 }
--- src/app/modules/empleados/empleados.service.ts
+++ src/app/modules/empleados/empleados.service.ts
@@ -1,54 +1,59 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
-import { IEmpleados } from './models/empleados.model';
+import { Empleados, IEmpleados } from './models/empleados.model';
 import { IResponse } from 'src/app/shared/models/response.model';
+import { Router } from '@angular/router';
+import { NotificacionesService } from 'src/app/shared/notificaciones.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class EmpleadosService {
-
   readonly APP = 'empleados';
   readonly API = `${environment.URL_API}/${this.APP}`;
-
   empleadosLista: IEmpleados[] = [];
-  empleadosSelect = '';
-  constructor(private http: HttpClient) {}
+  empleados: Empleados = new Empleados();
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+    private notificacionService: NotificacionesService
+  ) {}
 
   addEmpleados(empleados: IEmpleados) {
-    this.http
-      .post<IResponse>(this.API, empleados)
-      .subscribe((response: IResponse) => {
-        console.log(response.data);
-      });
+    this.http.post(this.API, empleados).subscribe((response) => {
+      this.notificacionService.alertOk('OK', 'Se guardo el empleado.');
+      this.getEmpleados();
+      this.router.navigate(['/app/modules/empleados/list']);
+    });
   }
 
   getEmpleados() {
     this.http.get<IResponse>(this.API).subscribe((response: IResponse) => {
+      console.log(response.data);
       this.empleadosLista = response.data;
     });
   }
 
-  getEmpleado() {
-    this.http.get<IResponse>(this.API).subscribe((response: IResponse) => {
-      console.log(response.data);
-    });
+  getEmpleado(id: string) {
+    return this.http.get(`${this.API}/${id}`);
   }
 
   updateEmpleados(empleados: IEmpleados) {
     this.http
       .put<IResponse>(this.API, empleados)
       .subscribe((response: IResponse) => {
-        console.log(response.data);
+        this.notificacionService.alertOk('OK', 'Se actualizo el empleado.');
+        this.router.navigate(['/app/modules/empleados/list']);
       });
   }
 
-  deleteEmpleados(id: string) {
+  deleteEmpleados(id: string | undefined) {
     this.http
       .delete<IResponse>(`${this.API}/${id}`)
       .subscribe((response: IResponse) => {
-        console.log(response.data);
+        this.notificacionService.alertOk('OK', 'Se elimino el empleado.');
+        this.getEmpleados();
       });
   }
 }
